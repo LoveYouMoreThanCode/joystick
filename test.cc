f9845b454460429896ba1f020269bde32946c065
fanwei <fanwei@expontech.com>
2024-02-20 17:51:19 +0800

    support direction command

--- test.cc
+++ test.cc
@@ -13,39 +13,82 @@
 // Copyright Drew Noakes 2013-2016
 
 #include "joystick.hh"
+#include <cassert>
 #include <unistd.h>
 
-int main(int argc, char** argv)
-{
+
+class JoystickDirCommand {
+public:
+  JoystickDirCommand(std::string path) : js_(path) {}
+  ~JoystickDirCommand() {}
+  std::string scan_cmd() {
+    assert(js_.isFound());
+    JoystickEvent event;
+    while (js_.sample(&event)) {
+      if (!event.isAxis()) {
+        continue;
+      }
+      if (event.number == 0) {
+        x_ = event.value;
+      } else if (event.number == 1) {
+        y_ = event.value;
+      }
+    }
+    return make_cmd();
+  }
+
+private:
+  std::string make_cmd() {
+    if (y_ == 0) {
+      return "brake";
+    }
+    if (y_ < 0) {
+      return "backward";
+    }
+    if (x_ == 0) {
+      return "forward";
+    } else if (x_ < 0) {
+      return "left";
+    } else {
+      return "right";
+    }
+  }
+
+private:
+  int x_{0};
+  int y_{0};
+  Joystick js_;
+};
+
+int main(int argc, char **argv) {
   // Create an instance of Joystick
   Joystick joystick("/dev/input/js0");
 
   // Ensure that it was found and that we can use it
-  if (!joystick.isFound())
-  {
+  if (!joystick.isFound()) {
     printf("open failed.\n");
     exit(1);
   }
 
-  while (true)
-  {
+  while (true) {
     // Restrict rate
     usleep(1000);
 
     // Attempt to sample an event from the joystick
     JoystickEvent event;
-    if (joystick.sample(&event))
-    {
-      if (event.isButton())
-      {
-        printf("Button %u is %s\n",
-          event.number,
-          event.value == 0 ? "up" : "down");
-      }
-      else if (event.isAxis())
-      {
+    if (joystick.sample(&event)) {
+      if (event.isButton()) {
+        printf("Button %u is %s\n", event.number,
+               event.value == 0 ? "up" : "down");
+      } else if (event.isAxis()) {
         printf("Axis %u is at position %d\n", event.number, event.value);
       }
     }
+  }
+
+  JoystickDirCommand js_cmd("/dev/input/js0");
+  while (true) {
+    std::string cmd = js_cmd.scan_cmd();
+    sleep(1);
   }
 }
