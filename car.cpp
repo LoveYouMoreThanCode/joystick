98be6251f876f9c241617cee41f97d999d6da2ce
fanwei <fanwei@expontech.com>
2024-02-22 09:29:11 +0800

    support pwm control

--- car.cpp
+++ car.cpp
@@ -1,6 +1,7 @@
+#include "commander.h"
 #include <iostream>
 #include <unistd.h>
-#include "commander.h"
+
 
 extern "C" {
 #include "lgpio.h"
@@ -9,6 +10,9 @@ extern "C" {
 #include <memory>
 #include <unordered_map>
 
+int lgTxPwm(int handle, int gpio, float pwmFrequency, float pwmDutyCycle,
+            int pwmOffset, int pwmCycles);
+#define MOTOR_DRIVE_PWM_FREQ_HZ 1000 /*Hz*/
 class Motor {
 public:
   Motor(int controller, const std::string &name, uint32_t p1, uint32_t p2)
@@ -25,26 +29,39 @@ public:
     }
     return 0;
   }
-  void move_forward() {
+  void move_forward(uint32_t speed) {
     std::cout << "motor:" << name_ << " move forward,"
-              << " ctl:" << ctl_ << " p1:" << p1_ << " p2:" << p2_ << std::endl;
+              << " ctl:" << ctl_ << " p1:" << p1_ << " p2:" << p2_
+              << " speed:" << speed << std::endl;
     // AT8236驱动方式：IN1=1 IN2=0 --> 正转
-    lgGpioWrite(ctl_, p1_, 1);
-    lgGpioWrite(ctl_, p2_, 0);
+    // lgGpioWrite(ctl_, p1_, 1);
+    // lgGpioWrite(ctl_, p2_, 0);
+    lgTxPwm(ctl_, p1_, MOTOR_DRIVE_PWM_FREQ_HZ, revise_speed(speed), 0, 0);
+    lgTxPwm(ctl_, p2_, 0, 0, 0);
   }
-  void move_backward() {
+  void move_backward(uint32_t speed) {
     std::cout << "motor:" << name_ << " move backward,"
-              << " ctl:" << ctl_ << " p1:" << p1_ << " p2:" << p2_ << std::endl;
+              << " ctl:" << ctl_ << " p1:" << p1_ << " p2:" << p2_
+              << " speed:" << speed << std::endl;
     // AT8236驱动方式：IN1=0 IN2=1 --> 反转
-    lgGpioWrite(ctl_, p1_, 0);
-    lgGpioWrite(ctl_, p2_, 1);
+    // lgGpioWrite(ctl_, p1_, 0);
+    // lgGpioWrite(ctl_, p2_, 1);
+    lgTxPwm(ctl_, p1_, 0, 0, 0);
+    lgTxPwm(ctl_, p2_, MOTOR_DRIVE_PWM_FREQ_HZ, revise_speed(speed), 0, 0);
   }
   void brake() {
     std::cout << "motor:" << name_ << " brake,"
               << " ctl:" << ctl_ << " p1:" << p1_ << " p2:" << p2_ << std::endl;
     // AT8236驱动方式：IN1=1 IN2=1 --> 刹车
-    lgGpioWrite(ctl_, p1_, 0);
-    lgGpioWrite(ctl_, p2_, 0);
+    // lgGpioWrite(ctl_, p1_, 0);
+    // lgGpioWrite(ctl_, p2_, 0);
+    lgTxPwm(ctl_, p1_, 0, 0, 0);
+    lgTxPwm(ctl_, p2_, 0, 0, 0);
+  }
+
+private:
+  uint32_t revise_speed(uint32_t speed) {
+    return std::min(100, std::max(20, speed));
   }
 
 private:
@@ -78,7 +95,7 @@ public:
     motors_["left_rear"] = left;
     motors_["right_rear"] = right;
 
-    //add two mor motors here
+    // add two mor motors here
     uint32_t left_front_p1 = 5;
     uint32_t left_front_p2 = 6;
     Motor left1(ctl_handle_, "left_front", left_front_p1, left_front_p2);
@@ -99,36 +116,35 @@ public:
     return 0;
   }
   void move_forward() {
-    motors_["left_rear"].move_forward();
-    motors_["right_rear"].move_forward();
-    motors_["left_front"].move_forward();
-    motors_["right_front"].move_forward();
+    motors_["left_rear"].move_forward(forward_speed_);
+    motors_["right_rear"].move_forward(forward_speed_);
+    motors_["left_front"].move_forward(forward_speed_);
+    motors_["right_front"].move_forward(forward_speed_);
   }
   void move_backward() {
-    motors_["left_rear"].move_backward();
-    motors_["right_rear"].move_backward();
-    motors_["left_front"].move_backward();
-    motors_["right_front"].move_backward();
+    motors_["left_rear"].move_backward(backward_speed_);
+    motors_["right_rear"].move_backward(backward_speed_);
+    motors_["left_front"].move_backward(backward_speed_);
+    motors_["right_front"].move_backward(backward_speed_);
   }
-  void turn_left(bool spin = false) {
+  void turn_left(bool spin = true) {
     spin = true;
-    motors_["left_rear"].move_forward();
-    motors_["left_front"].move_forward();
+    motors_["left_rear"].move_forward(turn_speed_);
+    motors_["left_front"].move_forward(turn_speed_);
     if (spin) {
-      motors_["right_rear"].move_backward();
-      motors_["right_front"].move_backward();
+      motors_["right_rear"].move_backward(turn_speed_);
+      motors_["right_front"].move_backward(turn_speed_);
     } else {
       motors_["right_rear"].brake();
       motors_["right_front"].brake();
     }
   }
-  void turn_right(bool spin = false) {
-    spin = true;
-    motors_["right_rear"].move_forward();
-    motors_["right_front"].move_forward();
+  void turn_right(bool spin = true) {
+    motors_["right_rear"].move_forward(turn_speed_);
+    motors_["right_front"].move_forward(turn_speed_);
     if (spin) {
-      motors_["left_rear"].move_backward();
-      motors_["left_front"].move_backward();
+      motors_["left_rear"].move_backward(turn_speed_);
+      motors_["left_front"].move_backward(turn_speed_);
     } else {
       motors_["left_rear"].brake();
       motors_["left_front"].brake();
@@ -144,27 +160,29 @@ public:
 private:
   int32_t ctl_handle_;
   std::unordered_map<std::string, Motor> motors_;
+  uint32_t forward_speed_{90};
+  uint32_t backward_speed_{50};
+  uint32_t turn_speed_{30};
 };
-
 
 int main() {
   Car my_car;
   int rc = my_car.init();
   if (rc) {
-    std::cout<<"failed to init my car, rc:"<<rc<<std::endl;
+    std::cout << "failed to init my car, rc:" << rc << std::endl;
     return rc;
   }
   std::unique_ptr<Commander, void (*)(Commander *)> commander(
       make_commander("joystick"), destroy_commander);
   while (true) {
-    //保持一定的控制周期
+    // 保持一定的控制周期
     lguSleep(0.1);
 
-    //命令输入提示
+    // 命令输入提示
     std::cout << std::endl << std::endl;
-    std::cout
-        << "...........等待输入指令left(l)/right(r)/forward(f)/backward(b)/brake(*)....."
-        << std::endl;
+    std::cout << "...........等待输入指令left(l)/right(r)/forward(f)/"
+                 "backward(b)/brake(*)....."
+              << std::endl;
     std::string cmd = commander->scan_cmd();
 
     if (cmd == "left" || cmd == "l") {
@@ -184,6 +202,6 @@ int main() {
       my_car.brake();
     }
   }
-  //结束
+  // 结束
   return 0;
 }
